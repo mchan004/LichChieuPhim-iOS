9f489372713253ccf4fd01558671d5ff7e724766
mchan004 <spqtthietthuc@gmail.com>
2017-10-25 21:23:10 +0700

    Fix1

--- LichChieuPhim/Controller/NowShowingController.swift
+++ LichChieuPhim/Controller/NowShowingController.swift
@@ -8,12 +8,36 @@
 
 import UIKit
 
-class NowShowingController: UIViewController {
+class NowShowingController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
